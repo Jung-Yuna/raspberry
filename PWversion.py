10246918d3cdd464f161fbe20901ab72655c59ec
Jung-Yuna <youna0518@naver.com>
2021-05-19 17:10:36 +0100

    1. 키패드로 #을 누르면 비밀번호입력이 clcd에 *로 표시됨 1-1. 비밀번호가 맞으면 'Door is OPEN', 비밀번호가 틀리면 'Wrong Password' 2. 세대번호를 입력하면 clcd에 숫자로 표시됨

---
+++ PWversion.py
@@ -0,0 +1,65 @@
+import RPi_I2C_driver
+import RPi.GPIO as GPIO 
+from keypad import keypad
+from time import *
+
+
+GPIO.setwarnings(False)
+
+mylcd = RPi_I2C_driver.lcd()
+
+
+if __name__ == '__main__':
+    # Initialize
+    kp = keypad(columnCount = 4)
+    
+     
+    sleep(1)
+  # waiting for a keypress
+    digit = None
+    while digit == None:
+        digit = kp.getKey()
+    print(digit)
+    
+        
+    if digit == '#':
+        sleep(0.5)
+        seq = []
+        mylcd.lcd_display_string(" Password:",1)
+            
+        for i in range(4):
+            digit = None
+            while digit == None:
+                digit = kp.getKey()
+            seq.append(digit)
+            mylcd.lcd_display_string_pos("*",1,0x0A + i)
+            sleep(0.4)
+            
+            print(digit)
+            sleep(0.5)
+        print(seq)
+        if seq == [1, 2, 3, 4]:
+            print ("Door is OPEN")
+            mylcd.lcd_display_string(' *Door is OPEN*',2)
+            
+        else:
+            print ("Wrong Password")
+            mylcd.lcd_display_string(' Wrong Password',2)
+           
+    else:
+        seq = []
+        mylcd.lcd_display_string_pos("HOME:",1,0x03)
+        for i in range(4):
+            digit = None
+            while digit == None:
+                digit = kp.getKey()
+            seq.append(digit)
+            mylcd.lcd_display_string_pos(str(digit),1,0x08 + i)
+            sleep(0.4)
+            
+            print(digit)
+            sleep(0.5)
+        print(seq)
+        mylcd.lcd_display_string('  ***CALLING***',2)
+   
+
